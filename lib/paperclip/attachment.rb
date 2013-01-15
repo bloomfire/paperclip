53f050c340324fa6d0f2bf6f3774d7b9ca11eafa
Patrick McKinnon <patrick@bloomfire.com>
2013-01-14 18:24:24 -0600

    - Fix leaky tempfiles

--- lib/paperclip/attachment.rb
+++ lib/paperclip/attachment.rb
@@ -406,10 +406,11 @@ module Paperclip
     def post_process_style(name, style) #:nodoc:
       begin
         raise RuntimeError.new("Style #{name} has no processors defined.") if style.processors.blank?
-        @queued_for_write[name] = style.processors.inject(@queued_for_write[:original]) do |file, processor|
+        processed = style.processors.inject(@queued_for_write[:original]) do |file, processor|
           Paperclip.processor(processor).make(file, style.processor_options, self)
         end
-        @queued_for_write[name] = Paperclip.io_adapters.for(@queued_for_write[name])
+        @queued_for_write[name] = Paperclip.io_adapters.for(processed)
+        processed.close! if processed.respond_to? :close!
       rescue Paperclip::Error => e
         log("An error was received while processing: #{e.inspect}")
         (@errors[:processing] ||= []) << e.message if @options[:whiny]
